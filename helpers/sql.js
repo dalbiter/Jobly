b10d730da75601816908da9254eccb1ce518cfe7
danx <rushn24@gmail.com>
2024-05-27 21:49:18 -0400

    Part two: added option to filter get /companies by minEmployee, maxEmploye, and name, added additional unit tests for company model and route tests for companies route

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,17 @@
 const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Generates the SQL code needed for a partial update query
+ * 
+ * Given an object with data to update {firstName: "Updated first name", lastName: "Updated last name"} 
+ * and an object that maps the JS keys to their SQL column names { firstName: "first_name", lastName: "last_name"}
+ * returns an object with corresponding values for use in parameterized SQL queries 
+ *  //{
+ *      setCols: '"first_name"=$1, "last_name"=$2',
+ *      values: ['Updated first name', 'Updated last name']
+ *     }
+ *   
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -5,7 +5,7 @@ describe("sqlForPartialUpdate", function () {
   test("works: 1 item", function () {
     const result = sqlForPartialUpdate(
         { f1: "v1" },
-        { f1: "f1"});
+        { f1: "f1", fF2: "f2" });
     expect(result).toEqual({
       setCols: "\"f1\"=$1",
       values: ["v1"],
